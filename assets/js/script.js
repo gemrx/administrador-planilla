12337c098c0c95249f7feea0f32e87f5d0697931
gemr <gemrdev@gmail.com>
2023-11-04 19:24:01 -0500

    updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,19 @@
 const navPages = document.querySelectorAll('.page');
 let currentPage = document.querySelector('.registrar');
 
+function valoresIniciales() {
+    // setear la fecha inicial
+    const inputFecha =  document.querySelector('#input-fecha');
+    let fechaActual = new Date();
+    let actualyear = fechaActual.getFullYear();
+    let mesActual = String(fechaActual.getMonth() + 1).padStart(2, '0');
+    let diaActual = String(fechaActual.getDate()).padStart(2, '0');
+    inputFecha.value = `${actualyear}-${mesActual}-${diaActual}`;   
+}
+
 // cargar el contenido principal
 document.addEventListener('DOMContentLoaded', () => {
-    $('main').load('assets/pages/registrar.html');
+    $('main').load('assets/pages/registrar.html', valoresIniciales);
 })
 
 // cambiar de pagina
