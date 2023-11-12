e4faa3b7bd1bf7cc8e9d0f685fc03c491f665385
RodolAgo06 <rodolfoabrego3333@gmail.com>
2023-11-11 23:45:08 -0500

    Funciones BD terminadas

--- modelos/modelo_empleado.php
+++ modelos/modelo_empleado.php
@@ -60,96 +60,106 @@
     function buscarEmpleado($cedula) {
         global $conexion;
 
-        $query = "SELECT * FROM generales WHERE cedula = '$cedula'";
-        $resultado = mysqli_query($conexion, $query);
+        $query = "SELECT * FROM generales WHERE cedula = ?";
+        $stmt = mysqli_prepare($conexion, $query);
+    
+        // Vincular el parámetro
+        mysqli_stmt_bind_param($stmt, 's', $cedula);
+    
+        // Ejecutar la consulta
+        mysqli_stmt_execute($stmt);
+    
+        // Obtener el resultado de la consulta
+        $resultado = mysqli_stmt_get_result($stmt);
     
         // Si se encontró el empleado, devolver un array asociativo con los datos, de lo contrario, devolver null
         return mysqli_num_rows($resultado) > 0 ? mysqli_fetch_assoc($resultado) : null;
-        // retornar un arreglo asociativo con los datos del empleado
     }
+    
 
     function modificarEmpleado($datosEmpleado) {
         global $conexion;
     
+        // Obtener la cédula del array de datos
         $cedula = $datosEmpleado['cedula'];
-        $nombre1 = $datosEmpleado['nombre1'];
-        $nombre2 = $datosEmpleado['nombre2'];
-        $apellido1 = $datosEmpleado['apellido1'];
-        $apellido2 = $datosEmpleado['apellido2'];
-        $estadoCivil = $datosEmpleado['estado_civil'];
-        $apellidoCasada = $datosEmpleado['apellido_casada'];
-        $usaApellidoCasada = $datosEmpleado['usa_apellido_casada'];
-        $fechaNacimiento = $datosEmpleado['fecha_nacimiento'];
-        $peso = $datosEmpleado['peso'];
-        $estatura = $datosEmpleado['estatura'];
-        $tipoSangre = $datosEmpleado['tipo_sangre'];
-        $condicionFisica = $datosEmpleado['condicion_fisica'];
-        $pais = $datosEmpleado['pais'];
-        $provincia = $datosEmpleado['provincia'];
-        $distrito = $datosEmpleado['distrito'];
-        $corregimiento = $datosEmpleado['corregimiento'];
-        $comunidad = $datosEmpleado['comunidad'];
-        $calle = $datosEmpleado['calle'];
-        $casa = $datosEmpleado['casa'];
-        $estado = $datosEmpleado['estado'];
     
-        $query = "UPDATE generales 
-                  SET nombre1 = '$nombre1', 
-                      nombre2 = '$nombre2', 
-                      apellido1 = '$apellido1', 
-                      apellido2 = '$apellido2', 
-                      estado_civil = '$estadoCivil', 
-                      apellido_casada = '$apellidoCasada', 
-                      usa_apellido_casada = '$usaApellidoCasada', 
-                      fecha_nacimiento = '$fechaNacimiento', 
-                      peso = '$peso', 
-                      estatura = '$estatura', 
-                      tipo_sangre = '$tipoSangre', 
-                      condicion_fisica = '$condicionFisica', 
-                      pais = '$pais', 
-                      provincia = '$provincia', 
-                      distrito = '$distrito', 
-                      corregimiento = '$corregimiento', 
-                      comunidad = '$comunidad', 
-                      calle = '$calle', 
-                      casa = '$casa', 
-                      estado = '$estado' 
-                  WHERE cedula = '$cedula'";
+        // Verificar si la cédula existe antes de intentar la actualización
+        $verificarCedulaQuery = "SELECT COUNT(*) as total FROM generales WHERE cedula = '$cedula'";
+        $resultadoVerificacion = mysqli_query($conexion, $verificarCedulaQuery);
     
-        $resultado = mysqli_query($conexion, $query);
+        if ($resultadoVerificacion) {
+            $fila = mysqli_fetch_assoc($resultadoVerificacion);
+            $totalEmpleados = $fila['total'];
     
-        return $resultado ? true : false;
+            // Verificar si la cédula existe en la base de datos
+            if ($totalEmpleados == 1) {
+                // Extraer los demás datos del array
+                $nombre1 = $datosEmpleado['nombre1'];
+                $nombre2 = $datosEmpleado['nombre2'];
+                $apellido1 = $datosEmpleado['apellido1'];
+                $apellido2 = $datosEmpleado['apellido2'];
+                $genero = $datosEmpleado['genero'];
+                $estadoCivil = $datosEmpleado['estado_civil'];
+                $apellidoCasada = $datosEmpleado['apellido_casada'];
+                $usaApellidoCasada = $datosEmpleado['usa_apellido_casada'];
+                $fechaNacimiento = $datosEmpleado['fecha_nacimiento'];
+                $peso = $datosEmpleado['peso'];
+                $estatura = $datosEmpleado['estatura'];
+                $tipoSangre = $datosEmpleado['tipo_sangre'];
+                $condicionFisica = $datosEmpleado['condicion_fisica'];
+                $pais = $datosEmpleado['pais'];
+                $provincia = $datosEmpleado['provincia'];
+                $distrito = $datosEmpleado['distrito'];
+                $corregimiento = $datosEmpleado['corregimiento'];
+                $comunidad = $datosEmpleado['comunidad'];
+                $calle = $datosEmpleado['calle'];
+                $casa = $datosEmpleado['casa'];
+                $estado = $datosEmpleado['estado'];
+    
+                // Construir la consulta de actualización
+                $query = "UPDATE generales 
+                          SET nombre1 = '$nombre1', 
+                              genero= '$genero',
+                              nombre2 = '$nombre2', 
+                              apellido1 = '$apellido1', 
+                              apellido2 = '$apellido2', 
+                              estado_civil = '$estadoCivil', 
+                              apellido_casada = '$apellidoCasada', 
+                              usa_apellido_casada = '$usaApellidoCasada', 
+                              fecha_nacimiento = '$fechaNacimiento', 
+                              peso = '$peso', 
+                              estatura = '$estatura', 
+                              tipo_sangre = '$tipoSangre', 
+                              condicion_fisica = '$condicionFisica', 
+                              pais = '$pais', 
+                              provincia = '$provincia', 
+                              distrito = '$distrito', 
+                              corregimiento = '$corregimiento', 
+                              comunidad = '$comunidad', 
+                              calle = '$calle', 
+                              casa = '$casa', 
+                              estado = '$estado' 
+                          WHERE cedula = '$cedula'";
+    
+                // Ejecutar la consulta de actualización
+                $resultado = mysqli_query($conexion, $query);
+    
+                return $resultado ? true : false;
+            } else {
+                // La cédula no existe en la base de datos
+                return false;
+            }
+        } else {
+            // Error al ejecutar la consulta de verificación
+            return false;
+        }
     }
     
+    
+    
+    
+    
 
 
-    $datos_registrar = array(
-        'prefijo' => '8',
-        'tomo' => '966',
-        'asiento' => '',
-        'genero' => '',
-        'cedula' => '',
-        'nombre1' => '',
-        'nombre2' => null,
-        'apellido1' => '',
-        'apellido2' => null,
-        'estado_civil' => '',
-        'apellido_casada' => null,
-        'usa_apellido_casada' => '',
-        'fecha_nacimiento' => 'YYYY-MM-DD',
-        'peso' => '',
-        'estatura' => '',
-        'tipo_sangre' => '',
-        'condicion_fisica' => '',
-        'pais' => '',
-        'provincia' => '',
-        'distrito' => '',
-        'corregimiento' => '',
-        'comunidad' => '',
-        'calle' => null,
-        'casa' => null,
-        'estado' => '1'
-    );
-
-    registrarEmpleado($datos_registrar);
+  
 ?>
--- prueba.php
+++ prueba.php
@@ -1,23 +1,105 @@
 <?php
    
     require(__DIR__ ."/modelos/modelo_empleado.php");
-    
-    $datosEmpleado = [
-        'prefijo' => '8',
-        'tomo' => '555',
-        'asiento' => '123',
-        'genero' => 'Masculino',
-        'cedula' => '123456789',
-        'nombre1' => 'Juan',
-        'nombre2' => 'Carlos',
+/*
+    $datosNuevoEmpleado = [
+        
+    'prefijo' => '8',
+    'tomo' => '990',
+    'asiento' => '1734',
+    'cedula' => '8-990-1734',
+    'genero' => 'M',
+    'nombre1' => 'Juan',
+    'nombre2' => 'Carlos',
+    'apellido1' => 'Gómez',
+    'apellido2' => 'Pérez',
+    'estado_civil' => 'Soltero',
+    'apellido_casada' => null,
+    'usa_apellido_casada' => 'No',
+    'fecha_nacimiento' => '1990-01-01',
+    'peso' => '70',
+    'estatura' => '175',
+    'tipo_sangre' => 'O+',
+    'condicion_fisica' => 'Buena',
+    'pais' => 'Panamá',
+    'provincia' => 'Panamá',
+    'distrito' => 'Panamá',
+    'corregimiento' => 'Bella Vista',
+    'comunidad' => 'ComunidadPrueba',
+    'calle' => 'CallePrueba',
+    'casa' => '123',
+    'estado' => 'Activo',
         // ... (otros campos)
     ];
-    $resultado = registrarEmpleado($datosEmpleado);
-
-    if ($resultado) {
+    
+    // Llamada a la función de registro
+    $resultadoRegistro = registrarEmpleado($datosNuevoEmpleado);
+    
+    // Mostrar el resultado
+    if ($resultadoRegistro) {
         echo "Empleado registrado correctamente.\n";
     } else {
         echo "Error al registrar el empleado.\n";
     }
-        
+     
+    
+   // Datos para la edición del empleado
+$datosEdicionEmpleado = [
+    
+    'prefijo' => '8',
+    'tomo' => '990',
+    'asiento' => '1734',
+    'cedula' => '8-990-1734',
+    'genero' => 'F',
+    'nombre1' => 'Juan',
+    'nombre2' => 'Carlos',
+    'apellido1' => 'Gómez',
+    'apellido2' => 'Pérez',
+    'estado_civil' => 'Soltero',
+    'apellido_casada' => null,
+    'usa_apellido_casada' => 'No',
+    'fecha_nacimiento' => '1990-01-01',
+    'peso' => '70',
+    'estatura' => '175',
+    'tipo_sangre' => 'O+',
+    'condicion_fisica' => 'Buena',
+    'pais' => 'Panamá',
+    'provincia' => 'Panamá',
+    'distrito' => 'Panamá',
+    'corregimiento' => 'Bella Vista',
+    'comunidad' => 'ComunidadPrueba',
+    'calle' => 'CallePrueba',
+    'casa' => '123',
+    'estado' => 'Activo',
+    
+];
+
+// Llamada a la función de edición
+$resultadoEdicion = modificarEmpleado($datosEdicionEmpleado);
+
+// Mostrar el resultado
+if ($resultadoEdicion) {
+    echo "Empleado editado correctamente.\n";
+} else {
+    echo "Error al editar el empleado.\n";
+    }
+    
+//Prueba para buscar Empleado
+    $cedulaParaPrueba = "8-990-1734";
+
+    // Llamada a la función de búsqueda
+    $resultadoBusqueda = buscarEmpleado($cedulaParaPrueba);
+    
+    // Mostrar el resultado
+    if ($resultadoBusqueda) {
+        echo "Cédula encontrada. Datos del empleado:\n";
+        print_r($resultadoBusqueda);
+    } else {
+        echo "Cédula no encontrada.\n";
+    }
+    ?>
+*/
+    
+    
+
 ?>
